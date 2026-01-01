bfda07cbaee61ea51547f614a435cb8d833b9427
Lazsqe00 <kidieu000@gmail.com>
2026-01-01 10:29:31 +0700

    Update Connection.cs

--- QuanLyQuanCafe/QuanLyQuanCafe/Connection.cs
+++ QuanLyQuanCafe/QuanLyQuanCafe/Connection.cs
@@ -10,7 +10,7 @@ namespace QuanLyQuanCafe
 {
     internal class Connection
     {
-        public static string connection = @"Data Source=LAPTOP-2D858Q81;Initial Catalog=QuanLyQuanCafe;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public static string connection = @"";
         public static SqlConnection GetSqlConnection()
         {
             return new SqlConnection(connection);
